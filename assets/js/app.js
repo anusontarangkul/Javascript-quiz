e3ff95326cdd1b2d8d86d030b7ba149ea7edd320
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-06-17 14:34:19 -0700

    have outline of start page

---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+// JS Questions and choices
+
+const questions = [
+    {
+        question: "Which one of these keywords is NOT a JavaScript data type?",
+        choice1: "undefined",
+        choice2: "boolean",
+        choice3: "dictionary",
+        choice4: "string",
+        answer: 1
+    },
+    {
+        question: "Which one of these keywords does NOT declare a new variable in JavaScript?",
+        choice1: "var",
+        choice2: "let",
+        choice3: "const",
+        choice4: "def",
+        answer: 4
+    },
+    {
+        question: "Which one of these data types is a reference value",
+        choice1: "object",
+        choice2: "symbol",
+        choice3: "string",
+        choice4: "boolean",
+        answer: 1
+    },
+    {
+        question: "Who created JavaScript?",
+        choice1: "Microsoft",
+        choice2: "Netscape",
+        choice3: "Sun Microsystems",
+        choice4: "Oracle",
+        answer: 3
+    },
+    {
+        question: "Which of the following is not a reserved word in JavaScript?",
+        choice1: "default",
+        choice2: "finally",
+        choice3: "throw",
+        choice4: "undefined",
+        answer: 1
+    },
+    {
+        question: "Which method removes the last element from an array and returns that element",
+        choice1: "forEach()",
+        choice2: "pop()",
+        choice3: "push()",
+        choice4: "Concat()",
+        answer: 2
+    },
+
+
+
+]
+
+console.log(questions)
+
